5cbb6ea841b87cd36be78bf543731af2dc784481
Vaidiknamdev <vaidiknamdev4530@gmail.com>
2026-02-25 00:44:45 +0530

    Initial commit - Spring Boot base setup with MySQL Docker configuration

---
+++ src/main/java/com/vaidik/ragbackend/RagbackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaidik.ragbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagbackendApplication.class, args);
+	}
+
+}
